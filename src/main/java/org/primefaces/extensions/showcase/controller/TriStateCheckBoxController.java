faf9ccd996cd792f275a3c9b469c0adbe9cecdca
Mauricio Fenoglio <fenoloco@gmail.com>
2012-02-14 23:47:22 +0000

    checking svn and template config :)

---
+++ src/main/java/org/primefaces/extensions/showcase/controller/TriStateCheckBoxController.java
@@ -0,0 +1,15 @@
+/*
+* $Id
+*/
+
+package org.primefaces.extensions.showcase.controller;
+
+/**
+ * 
+ * @author  Mauricio Fenoglio / last modified by $Author: $
+ * @version $Revision: $
+ * @since   0.3
+ */
+public class TriStateCheckBoxController {
+
+}
