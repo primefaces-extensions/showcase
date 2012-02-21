af60858e4eac2c24a2052ec50c8cd4e8f7e367bb
Oleg Varaksin <ovaraksin@googlemail.com>
2012-02-21 15:02:20 +0000

    Small fixes in showcase

--- src/main/java/org/primefaces/extensions/showcase/controller/TriStateCheckBoxController.java
+++ src/main/java/org/primefaces/extensions/showcase/controller/TriStateCheckBoxController.java
@@ -30,7 +30,7 @@ public class TriStateCheckBoxController implements Serializable {
 
 	private String value1;
 	private String value2;
-        private String value3;
+	private String value3;
 
 	private Boolean valueB1;
 	private Boolean valueB2;
@@ -51,14 +51,14 @@ public class TriStateCheckBoxController implements Serializable {
 		return value2;
 	}
 
-        public String getValue3() {
-            return value3;
-        }
+	public String getValue3() {
+		return value3;
+	}
 
-        public void setValue3(String value3) {
-            this.value3 = value3;
-        }
-        
+	public void setValue3(String value3) {
+		this.value3 = value3;
+	}
+
 	public void setValue2(String value2) {
 		this.value2 = value2;
 	}
@@ -80,7 +80,7 @@ public class TriStateCheckBoxController implements Serializable {
 	}
 
 	public void addMessage() {
-		String checkBoxValue = "State is = " + value2;
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(checkBoxValue));
+		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "State has been changed", "State is = " + value2);
+		FacesContext.getCurrentInstance().addMessage(null, msg);
 	}
 }
