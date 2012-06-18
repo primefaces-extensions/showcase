6264ff9cfbd82b568e96dfe7e7e25585b9f0db6d
Oleg Varaksin <ovaraksin@googlemail.com>
2012-06-18 09:06:40 +0000

    Themes 1.0.6

--- src/main/java/org/primefaces/extensions/showcase/model/system/AvailableThemes.java
+++ src/main/java/org/primefaces/extensions/showcase/model/system/AvailableThemes.java
@@ -1,86 +1,108 @@
+/*
+ * Copyright 2011-2012 PrimeFaces Extensions.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * $Id$
+ */
+
 package org.primefaces.extensions.showcase.model.system;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+
 /**
- * DOCUMENT_ME
+ * AvailableThemes
  *
  * @author  Oleg Varaksin / last modified by $Author$
  * @version $Revision$
  */
 public class AvailableThemes {
 
-	private static AvailableThemes instance = null;
+    private static AvailableThemes instance = null;
 
-	public static AvailableThemes getInstance() {
-		if (instance == null) {
-			instance = new AvailableThemes();
-		}
+    private final HashMap<String, Theme> themesAsMap;
+    private final List<Theme> themes;
 
-		return instance;
-	}
+    private AvailableThemes() {
+        final List<String> themeNames = new ArrayList<String>();
 
-	private final HashMap<String, Theme> themesAsMap;
-	private final List<Theme> themes;
+        themeNames.add("afterdark");
+        themeNames.add("afternoon");
+        themeNames.add("afterwork");
+        themeNames.add("aristo");
+        themeNames.add("black-tie");
+        themeNames.add("blitzer");
+        themeNames.add("bluesky");
+        themeNames.add("casablanca");
+        themeNames.add("cruze");
+        themeNames.add("cupertino");
+        themeNames.add("dark-hive");
+        themeNames.add("dot-luv");
+        themeNames.add("eggplant");
+        themeNames.add("excite-bike");
+        themeNames.add("flick");
+        themeNames.add("glass-x");
+        themeNames.add("home");
+        themeNames.add("hot-sneaks");
+        themeNames.add("humanity");
+        themeNames.add("le-frog");
+        themeNames.add("midnight");
+        themeNames.add("mint-choc");
+        themeNames.add("overcast");
+        themeNames.add("pepper-grinder");
+        themeNames.add("redmond");
+        themeNames.add("rocket");
+        themeNames.add("sam");
+        themeNames.add("smoothness");
+        themeNames.add("south-street");
+        themeNames.add("start");
+        themeNames.add("swanky-purse");
+        themeNames.add("trontastic");
+        themeNames.add("ui-darkness");
+        themeNames.add("ui-lightness");
+        themeNames.add("vader");
 
-	private AvailableThemes() {
-		final List<String> themeNames = new ArrayList<String>();
+        themesAsMap = new HashMap<String, Theme>();
+        themes = new ArrayList<Theme>();
 
-		themeNames.add("afterdark");
-		themeNames.add("afternoon");
-		themeNames.add("afterwork");
-		themeNames.add("aristo");
-		themeNames.add("black-tie");
-		themeNames.add("blitzer");
-		themeNames.add("bluesky");
-		themeNames.add("casablanca");
-		themeNames.add("cupertino");
-		themeNames.add("dark-hive");
-		themeNames.add("dot-luv");
-		themeNames.add("eggplant");
-		themeNames.add("excite-bike");
-		themeNames.add("flick");
-		themeNames.add("glass-x");
-		themeNames.add("home");
-		themeNames.add("hot-sneaks");
-		themeNames.add("humanity");
-		themeNames.add("le-frog");
-		themeNames.add("midnight");
-		themeNames.add("mint-choc");
-		themeNames.add("overcast");
-		themeNames.add("pepper-grinder");
-		themeNames.add("redmond");
-		themeNames.add("rocket");
-		themeNames.add("sam");
-		themeNames.add("smoothness");
-		themeNames.add("south-street");
-		themeNames.add("start");
-		themeNames.add("swanky-purse");
-		themeNames.add("trontastic");
-		themeNames.add("ui-darkness");
-		themeNames.add("ui-lightness");
-		themeNames.add("vader");
+        for (final String themeName : themeNames) {
+            final Theme theme = new Theme();
+            theme.setName(themeName);
+            theme.setImage("/resources/images/themeswitcher/" + themeName +
+                ".png");
 
-		themesAsMap = new HashMap<String, Theme>();
-		themes = new ArrayList<Theme>();
+            themes.add(theme);
+            themesAsMap.put(theme.getName(), theme);
+        }
+    }
 
-		for (final String themeName : themeNames) {
-			final Theme theme = new Theme();
-			theme.setName(themeName);
-			theme.setImage("/resources/images/themeswitcher/" + themeName + ".png");
+    public static AvailableThemes getInstance() {
 
-			themes.add(theme);
-			themesAsMap.put(theme.getName(), theme);
-		}
-	}
+        if (instance == null) {
+            instance = new AvailableThemes();
+        }
 
-	public final List<Theme> getThemes() {
-		return themes;
-	}
+        return instance;
+    }
 
-	public Theme getThemeForName(final String name) {
-		return themesAsMap.get(name);
-	}
+    public final List<Theme> getThemes() {
+        return themes;
+    }
+
+    public Theme getThemeForName(final String name) {
+        return themesAsMap.get(name);
+    }
 }
