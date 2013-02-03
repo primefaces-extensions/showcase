a954a0b342272c9858475c6c69c69ec5edf06b9f
Oleg Varaksin <ovaraksin@googlemail.com>
2013-02-03 19:12:50 +0100

    Better formatting for exporter

--- src/main/java/org/primefaces/extensions/showcase/controller/DynamicColumnsTableController.java
+++ src/main/java/org/primefaces/extensions/showcase/controller/DynamicColumnsTableController.java
@@ -18,191 +18,186 @@
 
 package org.primefaces.extensions.showcase.controller;
 
-import java.io.IOException;
 import java.io.Serializable;
-import java.lang.System;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
-import javax.faces.component.UIComponent;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import java.util.*;
 
 /**
  * DynamicColumnsTableController
  *
- * @author Sudheer Jonna / last modified by $Author$
- * @since 0.7.0
+ * @author  Sudheer Jonna / last modified by $Author$
+ * @since   0.7.0
+ * @version $Revision: 1.0 $
  */
 
 @ManagedBean
 @ViewScoped
 public class DynamicColumnsTableController implements Serializable {
 
-    private final static List<String> VALID_COLUMN_KEYS = Arrays.asList("subject", "text", "country", "textLength", "deliveryStatus");
+	private static final List<String> VALID_COLUMN_KEYS =
+	    Arrays.asList("subject", "text", "country", "textLength", "deliveryStatus");
 
-    private static final long serialVersionUID = 20111020L;
+	private static final long serialVersionUID = 20111020L;
 
-    private List<Message> messages;
-    private List<Message> messageDetails;
-    private List<Message> filteredMessages;
+	private List<Message> messages;
+	private List<Message> messageDetails;
+	private List<Message> filteredMessages;
 
-    private String columnTemplate = "subject text country textLength";
-    private List<ColumnModel> columns = new ArrayList<ColumnModel>();
+	private String columnTemplate = "subject text country textLength";
+	private List<ColumnModel> columns = new ArrayList<ColumnModel>();
 
-    public DynamicColumnsTableController() {
-        if (messages == null) {
-            messages = new ArrayList<Message>();
+	public DynamicColumnsTableController() {
+		if (messages == null) {
+			messages = new ArrayList<Message>();
 
-            for (int i = 0; i < 10; i++) {
-                Message message = new Message();
-                message.setSubject("subject " + i);
-                message.setText("text " + i);
-                message.setTextLength(((i * 10) + 10) + "");
-                message.setCountry("country" + i);
-                message.setDeliveryStatus("successfull");
-                messages.add(message);
-            }
+			for (int i = 0; i < 10; i++) {
+				Message message = new Message();
+				message.setSubject("subject " + i);
+				message.setText("text " + i);
+				message.setTextLength(((i * 10) + 10) + "");
+				message.setCountry("country" + i);
+				message.setDeliveryStatus("successfull");
+				messages.add(message);
+			}
+		}
 
-        }
+		createDynamicColumns();
+	}
 
-        createDynamicColumns();
-    }
+	private String getRandomModel() {
+		return UUID.randomUUID().toString().substring(0, 8);
+	}
+
+	public List<ColumnModel> getColumns() {
+		return columns;
+	}
+
+	public List<Message> getMessages() {
+		return messages;
+	}
+
+	public void setMessages(final List<Message> messages) {
+		this.messages = messages;
+	}
+
+	public List<Message> getFilteredMessages() {
+		return filteredMessages;
+	}
+
+	public void setFilteredMessages(List<Message> filteredMessages) {
+		this.filteredMessages = filteredMessages;
+	}
+
+	public static class ColumnModel implements Serializable {
+
+		private String header;
+		private String property;
+
+		public ColumnModel(String header, String property) {
+			this.header = header;
+			this.property = property;
+		}
+
+		public String getHeader() {
+			return header;
+		}
+
+		public String getProperty() {
+			return property;
+		}
+	}
 
 
-    private String getRandomModel() {
-        return UUID.randomUUID().toString().substring(0, 8);
-    }
+	public class Message implements Serializable {
 
-    public List<ColumnModel> getColumns() {
-        return columns;
-    }
+		private String subject;
+		private String text;
+		private long time;
+		private String textLength;
+		private String country;
+		private String deliveryStatus;
 
-    public List<Message> getMessages() {
-        return messages;
-    }
+		public Message() {
+			time = System.currentTimeMillis() + (long) (Math.random() * 10);
+			textLength = Math.random() * 10 + "";
+		}
 
-    public void setMessages(final List<Message> messages) {
-        this.messages = messages;
-    }
+		public final String getSubject() {
+			return subject;
+		}
 
-    public List<Message> getFilteredMessages() {
-        return filteredMessages;
-    }
+		public final void setSubject(String subject) {
+			this.subject = subject;
+		}
 
-    public void setFilteredMessages(List<Message> filteredMessages) {
-        this.filteredMessages = filteredMessages;
-    }
+		public final String getText() {
+			return text;
+		}
 
+		public final void setText(String text) {
+			this.text = text;
+		}
 
-    static public class ColumnModel implements Serializable {
+		public long getTime() {
+			return time;
+		}
 
-        private String header;
-        private String property;
+		public void setTime(long time) {
+			this.time = time;
+		}
 
-        public ColumnModel(String header, String property) {
-            this.header = header;
-            this.property = property;
-        }
+		public String getTextLength() {
+			return textLength;
+		}
 
-        public String getHeader() {
-            return header;
-        }
+		public void setTextLength(String textLength) {
+			this.textLength = textLength;
+		}
 
-        public String getProperty() {
-            return property;
-        }
-    }
+		public String getCountry() {
+			return country;
+		}
 
-    public class Message implements Serializable {
+		public void setCountry(String country) {
+			this.country = country;
+		}
 
-        private String subject;
-        private String text;
-        private long time;
-        private String textLength;
-        private String country;
-        private String deliveryStatus;
+		public String getDeliveryStatus() {
+			return deliveryStatus;
+		}
 
-        public Message() {
-            time = System.currentTimeMillis() + (long) (Math.random() * 10);
-            textLength = Math.random() * 10 + "";
-        }
+		public void setDeliveryStatus(String deliveryStatus) {
+			this.deliveryStatus = deliveryStatus;
+		}
+	}
 
-        public final String getSubject() {
-            return subject;
-        }
+	public String getColumnTemplate() {
+		return columnTemplate;
+	}
 
-        public final void setSubject(String subject) {
-            this.subject = subject;
-        }
+	public void setColumnTemplate(String columnTemplate) {
+		this.columnTemplate = columnTemplate;
+	}
 
-        public final String getText() {
-            return text;
-        }
+	public void updateColumns() {
+		createDynamicColumns();
+	}
 
-        public final void setText(String text) {
-            this.text = text;
-        }
+	public void createDynamicColumns() {
+		String[] columnKeys = columnTemplate.split(" ");
+		columns.clear();
 
-        public long getTime() {
-            return time;
-        }
+		for (String columnKey : columnKeys) {
+			String key = columnKey.trim();
 
-        public void setTime(long time) {
-            this.time = time;
-        }
-
-        public String getTextLength() {
-            return textLength;
-        }
-
-        public void setTextLength(String textLength) {
-            this.textLength = textLength;
-        }
-
-        public String getCountry() {
-            return country;
-        }
-
-        public void setCountry(String country) {
-            this.country = country;
-        }
-
-        public String getDeliveryStatus() {
-            return deliveryStatus;
-        }
-
-        public void setDeliveryStatus(String deliveryStatus) {
-            this.deliveryStatus = deliveryStatus;
-        }
-    }
-
-    public String getColumnTemplate() {
-        return columnTemplate;
-    }
-
-    public void setColumnTemplate(String columnTemplate) {
-        this.columnTemplate = columnTemplate;
-    }
-
-    public void updateColumns() {
-        createDynamicColumns();
-    }
-
-    public void createDynamicColumns() {
-        String[] columnKeys = columnTemplate.split(" ");
-        columns.clear();
-
-        for (String columnKey : columnKeys) {
-            String key = columnKey.trim();
-
-            if (VALID_COLUMN_KEYS.contains(key)) {
-                columns.add(new ColumnModel(columnKey.toUpperCase(), columnKey));
-            }
-        }
-    }
+			if (VALID_COLUMN_KEYS.contains(key)) {
+				columns.add(new ColumnModel(columnKey.toUpperCase(), columnKey));
+			}
+		}
+	}
 }
