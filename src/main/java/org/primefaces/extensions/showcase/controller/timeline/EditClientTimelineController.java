85f61123a9402b1e3c87a4770e909b44efdbce2a
Oleg Varaksin <ovaraksin@googlemail.com>
2013-03-30 23:31:15 +0100

    More work on Timeline

--- src/main/java/org/primefaces/extensions/showcase/controller/timeline/EditClientTimelineController.java
+++ src/main/java/org/primefaces/extensions/showcase/controller/timeline/EditClientTimelineController.java
@@ -21,35 +21,23 @@ import java.io.Serializable;
 import java.util.Calendar;
 
 import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
 
-import org.primefaces.extensions.event.timeline.TimelineSelectEvent;
 import org.primefaces.extensions.model.timeline.TimelineEvent;
 import org.primefaces.extensions.model.timeline.TimelineModel;
 
 /**
- * BasicTimelineController
+ * EditClientTimelineController
  *
  * @author  Oleg Varaksin / last modified by $Author: $
  * @version $Revision: 1.0 $
  */
 @ManagedBean
 @ViewScoped
-public class BasicTimelineController implements Serializable {
+public class EditClientTimelineController implements Serializable {
 
 	private TimelineModel model;
-
-	private boolean selectable = true;
-	private boolean zoomable = true;
-	private boolean moveable = true;
-	private boolean themable = false;
-	private String eventStyle = "box";
-	private boolean axisOnTop;
-	private boolean showCurrentTime = true;
-	private boolean showNavigation = false;
 
 	@PostConstruct
 	protected void initialize() {
@@ -87,78 +75,7 @@ public class BasicTimelineController implements Serializable {
 		model.add(new TimelineEvent("Primefaces-Extensions 0.6.3", cal.getTime()));
 	}
 
-	public void onSelect(TimelineSelectEvent e) {
-		TimelineEvent timelineEvent = e.getTimelineEvent();
-
-		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Selected event:", timelineEvent.getData().toString());
-		FacesContext.getCurrentInstance().addMessage(null, msg);
-	}
-
 	public TimelineModel getModel() {
 		return model;
-	}
-
-	public boolean isSelectable() {
-		return selectable;
-	}
-
-	public void setSelectable(boolean selectable) {
-		this.selectable = selectable;
-	}
-
-	public boolean isZoomable() {
-		return zoomable;
-	}
-
-	public void setZoomable(boolean zoomable) {
-		this.zoomable = zoomable;
-	}
-
-	public boolean isMoveable() {
-		return moveable;
-	}
-
-	public void setMoveable(boolean moveable) {
-		this.moveable = moveable;
-	}
-
-	public boolean isThemable() {
-		return themable;
-	}
-
-	public void setThemable(boolean themable) {
-		this.themable = themable;
-	}
-
-	public String getEventStyle() {
-		return eventStyle;
-	}
-
-	public void setEventStyle(String eventStyle) {
-		this.eventStyle = eventStyle;
-	}
-
-	public boolean isAxisOnTop() {
-		return axisOnTop;
-	}
-
-	public void setAxisOnTop(boolean axisOnTop) {
-		this.axisOnTop = axisOnTop;
-	}
-
-	public boolean isShowCurrentTime() {
-		return showCurrentTime;
-	}
-
-	public void setShowCurrentTime(boolean showCurrentTime) {
-		this.showCurrentTime = showCurrentTime;
-	}
-
-	public boolean isShowNavigation() {
-		return showNavigation;
-	}
-
-	public void setShowNavigation(boolean showNavigation) {
-		this.showNavigation = showNavigation;
 	}
 }
