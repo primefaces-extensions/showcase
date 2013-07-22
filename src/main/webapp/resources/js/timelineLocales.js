fabec52bda7e4d8c02e8f3e301b7a7d9a8f5bf1b
ova2 <ovaraksin@googlemail.com>
2013-07-22 15:41:05 +0200

    Added Italian locale for Timeline

--- src/main/webapp/resources/js/timelineLocales.js
+++ src/main/webapp/resources/js/timelineLocales.js
@@ -50,4 +50,17 @@ $(function() {
         'NEW': "Nuevo",
         'CREATE_NEW_EVENT': "Crear nuevo evento"
     };
+    
+    PrimeFacesExt.locales.Timeline['it'] = {
+        'MONTHS': ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
+        'MONTHS_SHORT': ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"],
+        'DAYS': ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato"],
+        'DAYS_SHORT': ["Dom", "Lun", "Mar", "Mer", "Gio", "Ven", "Sab"],
+        'ZOOM_IN': "Aumentare lo zoom",
+        'ZOOM_OUT': "Diminuire lo zoom",
+        'MOVE_LEFT': "Muovere a sinistra",
+        'MOVE_RIGHT': "Muovere a destra",
+        'NEW': "Nuovo",
+        'CREATE_NEW_EVENT': "Creare nuovo evento"
+    };    
 });
