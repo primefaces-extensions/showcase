b597706e678bc5a95fc9c4e93fb0d28973feabaf
Oleg Varaksin <ovaraksin@googlemail.com>
2013-11-01 19:31:56 +0100

    Implemented first example for FluidGrid

--- src/main/java/org/primefaces/extensions/showcase/webapp/UserSettings.java
+++ src/main/java/org/primefaces/extensions/showcase/webapp/UserSettings.java
@@ -43,7 +43,7 @@ public class UserSettings implements Serializable {
 	private Theme currentTheme;
 
 	public UserSettings() {
-		currentTheme = AvailableThemes.getInstance().getThemeForName("afterwork");
+		currentTheme = AvailableThemes.getInstance().getThemeForName("aristo");
 		availableThemes = AvailableThemes.getInstance().getThemes();
 	}
 
