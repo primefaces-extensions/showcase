90d3141cd019b9f50927cba825140a2fffa69361
francesco-strazzullo <me@francescostrazzullo.info>
2014-09-08 22:03:59 +0200

    Added Organizational Chart example

---
+++ src/main/java/org/primefaces/extensions/showcase/controller/gchart/OrganizationalGChartController.java
@@ -0,0 +1,54 @@
+package org.primefaces.extensions.showcase.controller.gchart;
+
+import org.primefaces.extensions.component.gchart.model.GChartModel;
+import org.primefaces.extensions.component.gchart.model.GChartModelBuilder;
+import org.primefaces.extensions.component.gchart.model.GChartType;
+import org.primefaces.model.DefaultTreeNode;
+import org.primefaces.model.TreeNode;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.io.Serializable;
+
+@ManagedBean
+@RequestScoped
+public class OrganizationalGChartController implements Serializable {
+
+    private static final long serialVersionUID = 253762400419864192L;
+
+    private GChartModel standardModel = null;
+    private GChartModel treeModel = null;
+
+    public GChartModel getStandardModel(){
+        return standardModel;
+    }
+
+    public GChartModel getTreeModel() {
+        return treeModel;
+    }
+
+    @PostConstruct
+    public void generateModel() {
+        standardModel = new GChartModelBuilder().setChartType(GChartType.ORGANIZATIONAL)
+                .addColumns("Name","Manager")
+                .addRow("Mike", "")
+                .addRow("Alice", "Mike")
+                .addRow("Jim", "Mike")
+                .addRow("Bob", "Alice")
+                .addOption("size", "large")
+                .build();
+
+        TreeNode mike = new DefaultTreeNode("Mike");
+        TreeNode alice = new DefaultTreeNode("Alice",mike);
+        TreeNode jim = new DefaultTreeNode("Jim",mike);
+        TreeNode bob = new DefaultTreeNode("Bob",alice);
+
+        treeModel = new GChartModelBuilder().setChartType(GChartType.ORGANIZATIONAL)
+                .addColumns("Name","Manager")
+                .importTreeNode(mike)
+                .addOption("size", "large")
+                .build();
+    }
+
+}
