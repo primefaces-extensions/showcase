f62d51fa3c081ff2777cda14bbfcf9588efda256
melloware <mellowaredev@gmail.com>
2016-11-28 09:23:40 -0500

    Added deprecated option for components in menu in addition to new and updated.

--- src/main/java/org/primefaces/extensions/showcase/webapp/TechnicalInfo.java
+++ src/main/java/org/primefaces/extensions/showcase/webapp/TechnicalInfo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011-2015 PrimeFaces Extensions
+ * Copyright 2011-2016 PrimeFaces Extensions
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,14 +15,8 @@
  *
  * $Id$
  */
-
 package org.primefaces.extensions.showcase.webapp;
 
-import javax.annotation.PostConstruct;
-import javax.faces.bean.ApplicationScoped;
-import javax.faces.bean.ManagedBean;
-import javax.faces.context.FacesContext;
-import javax.servlet.ServletContext;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -35,117 +29,133 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
 
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
+
 /**
  * TechnicalInfo.
  *
- * @author  Oleg Varaksin / last modified by $Author$
+ * @author Oleg Varaksin / last modified by Melloware
  * @version $Revision$
  */
 @ApplicationScoped
 @ManagedBean(eager = true)
 public class TechnicalInfo {
 
-	private static final Logger LOGGER = Logger.getLogger(TechnicalInfo.class.getName());
-	private String primeFaces;
-	private String primeFacesExt;
-	private String jsfImpl;
-	private String server;
-	private String buildTime;
-	private boolean mojarra = true;
+   private static final Logger LOGGER = Logger.getLogger(TechnicalInfo.class.getName());
+   private String primeFaces;
+   private String primeFacesExt;
+   private String jsfImpl;
+   private String server;
+   private String buildTime;
+   private boolean mojarra = true;
 
-	private List<String> newComponents = new ArrayList<String>();
-	private List<String> updatedComponents = new ArrayList<String>();
+   private List<String> newComponents = new ArrayList<String>();
+   private List<String> updatedComponents = new ArrayList<String>();
+   private List<String> deprecatedComponents = new ArrayList<String>();
 
-	@PostConstruct
-	protected void initialize() {
-		ResourceBundle rb;
-		try {
-			rb = ResourceBundle.getBundle("pe-showcase");
+   @PostConstruct
+   protected void initialize() {
+      ResourceBundle rb;
+      try {
+         rb = ResourceBundle.getBundle("pe-showcase");
 
-			String strAppProps = rb.getString("application.properties");
-			int lastBrace = strAppProps.indexOf("}");
-			strAppProps = strAppProps.substring(1, lastBrace);
+         String strAppProps = rb.getString("application.properties");
+         final int lastBrace = strAppProps.indexOf("}");
+         strAppProps = strAppProps.substring(1, lastBrace);
 
-			Map<String, String> appProperties = new HashMap<String, String>();
-			String[] appProps = strAppProps.split("[\\s,]+");
-			for (String appProp : appProps) {
-				String[] keyValue = appProp.split("=");
-				if (keyValue != null && keyValue.length > 1) {
-					appProperties.put(keyValue[0], keyValue[1]);
-				}
-			}
+         final Map<String, String> appProperties = new HashMap<String, String>();
+         final String[] appProps = strAppProps.split("[\\s,]+");
+         for (final String appProp : appProps) {
+            final String[] keyValue = appProp.split("=");
+            if (keyValue != null && keyValue.length > 1) {
+               appProperties.put(keyValue[0], keyValue[1]);
+            }
+         }
 
-			primeFaces = "PrimeFaces: " + appProperties.get("primefaces.core.version");
-			primeFacesExt = "PrimeFaces Extensions: " + appProperties.get("primefaces-extensions.core.version");
-			jsfImpl = "JSF: " + appProperties.get("jsf-impl") + " " + appProperties.get("jsf-version");
-			server =
-			    "Server: "
-			    + ((ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext()).getServerInfo();
+         primeFaces = "PrimeFaces: " + appProperties.get("primefaces.core.version");
+         primeFacesExt = "PrimeFaces Extensions: " + appProperties.get("primefaces-extensions.core.version");
+         jsfImpl = "JSF: " + appProperties.get("jsf-impl") + " " + appProperties.get("jsf-version");
+         server = "Server: "
+                  + ((ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext())
+                           .getServerInfo();
 
-			DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-			Calendar calendar = Calendar.getInstance();
+         final DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+         final Calendar calendar = Calendar.getInstance();
 
-			if (appProperties.containsKey("timestamp")) {
-				calendar.setTimeInMillis(Long.valueOf(appProperties.get("timestamp")));
-			}
+         if (appProperties.containsKey("timestamp")) {
+            calendar.setTimeInMillis(Long.valueOf(appProperties.get("timestamp")));
+         }
 
-			buildTime = "Build time: " + formatter.format(calendar.getTime());
-			mojarra = appProperties.get("jsf-impl").contains("mojarra");
+         buildTime = "Build time: " + formatter.format(calendar.getTime());
+         mojarra = appProperties.get("jsf-impl").contains("mojarra");
 
-			//mfenoglio process new and updated components
-			this.proccessNewsComponents(appProperties.get("primefaces-extensions.new-components"),
-			                            appProperties.get("primefaces-extensions.updated-components"));
-		} catch (MissingResourceException e) {
-			LOGGER.warning("Resource bundle 'pe-showcase' was not found");
-		}
-	}
+         // process new and updated components
+         processComponentTypes(appProperties.get("primefaces-extensions.new-components"),
+                  appProperties.get("primefaces-extensions.updated-components"),
+                  appProperties.get("primefaces-extensions.deprecated-components"));
+      } catch (final MissingResourceException e) {
+         LOGGER.warning("Resource bundle 'pe-showcase' was not found");
+      }
+   }
 
-	public String getPrimeFaces() {
-		return primeFaces;
-	}
+   public String getPrimeFaces() {
+      return primeFaces;
+   }
 
-	public String getPrimeFacesExt() {
-		return primeFacesExt;
-	}
+   public String getPrimeFacesExt() {
+      return primeFacesExt;
+   }
 
-	public String getJsfImpl() {
-		return jsfImpl;
-	}
+   public String getJsfImpl() {
+      return jsfImpl;
+   }
 
-	public String getServer() {
-		return server;
-	}
+   public String getServer() {
+      return server;
+   }
 
-	public String getBuildTime() {
-		return buildTime;
-	}
+   public String getBuildTime() {
+      return buildTime;
+   }
 
-	public boolean isMojarra() {
-		return mojarra;
-	}
+   public boolean isMojarra() {
+      return mojarra;
+   }
 
-	public String getMenuitemIconStyleClass(final String page) {
-		if (newComponents.contains(page)) {
-			return "ui-icon-new-comp";
-		}
+   public String getMenuitemIconStyleClass(final String page) {
+      if (newComponents.contains(page)) {
+         return "ui-icon-new-comp";
+      }
 
-		if (updatedComponents.contains(page)) {
-			return "ui-icon-updated-comp";
-		}
+      if (updatedComponents.contains(page)) {
+         return "ui-icon-updated-comp";
+      }
+      
+      if (deprecatedComponents.contains(page)) {
+         return "ui-icon-deprecated-comp";
+      }
 
-		return "ui-icon-none";
-	}
+      return "ui-icon-none";
+   }
 
-	private void proccessNewsComponents(final String newComp, final String updatedComp) {
-		try {
-			String[] newCompArray = newComp.split(";");
-			Collections.addAll(newComponents, newCompArray);
+   private void processComponentTypes(final String newComp, final String updatedComp, final String deprecatedComp) {
+      try {
+         final String[] newCompArray = newComp.split(";");
+         Collections.addAll(newComponents, newCompArray);
 
-			String[] updatedCompArray = updatedComp.split(";");
-			Collections.addAll(updatedComponents, updatedCompArray);
-		} catch (Exception ex) {
-			this.newComponents = new ArrayList<String>();
-			this.updatedComponents = new ArrayList<String>();
-		}
-	}
+         final String[] updatedCompArray = updatedComp.split(";");
+         Collections.addAll(updatedComponents, updatedCompArray);
+         
+         final String[] deprecatedCompArray = deprecatedComp.split(";");
+         Collections.addAll(deprecatedComponents, deprecatedCompArray);
+      } catch (final Exception ex) {
+         newComponents = new ArrayList<String>();
+         updatedComponents = new ArrayList<String>();
+         deprecatedComponents = new ArrayList<String>();
+      }
+   }
 }
