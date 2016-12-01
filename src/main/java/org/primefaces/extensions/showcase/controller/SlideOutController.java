dac1e5aa569b64d684046565613b7273053fdec2
melloware <mellowaredev@gmail.com>
2016-12-01 16:20:02 -0500

    Slideout ajax events for open and close added.

--- src/main/java/org/primefaces/extensions/showcase/controller/SlideOutController.java
+++ src/main/java/org/primefaces/extensions/showcase/controller/SlideOutController.java
@@ -2,8 +2,13 @@ package org.primefaces.extensions.showcase.controller;
 
 import java.io.Serializable;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.extensions.event.CloseEvent;
+import org.primefaces.extensions.event.OpenEvent;
 
 /**
  * SlideOut Controller.
@@ -15,5 +20,17 @@ import javax.faces.bean.ViewScoped;
 public class SlideOutController implements Serializable {
 
    private static final long serialVersionUID = 20120224L;
+
+   public void closeListener(final CloseEvent closeEvent) {
+      final FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Close fired",
+               "Component id: " + closeEvent.getComponent().getId());
+      FacesContext.getCurrentInstance().addMessage(null, msg);
+   }
+
+   public void openListener(final OpenEvent openEvent) {
+      final FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Open fired",
+               "Component id: " + openEvent.getComponent().getId());
+      FacesContext.getCurrentInstance().addMessage(null, msg);
+   }
 
 }
