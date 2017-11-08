91efc3e624c35889d9d6e93d1ec6b2565567089f
melloware <mellowaredev@gmail.com>
2017-11-08 09:12:13 -0500

    Fix #502: SpeedTest component examples updated.

--- src/main/java/org/primefaces/extensions/showcase/controller/speedtest/SpeedTestController.java
+++ src/main/java/org/primefaces/extensions/showcase/controller/speedtest/SpeedTestController.java
@@ -1,44 +1,46 @@
 package org.primefaces.extensions.showcase.controller.speedtest;
 
-/**
- * {@link Speedtest} Controller.
- *
- * @author Stefan Sibitz ssibitz@me.com
- */
-import org.primefaces.extensions.event.SpeedTestEvent;
+import java.io.Serializable;
+import java.util.Random;
 
-import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletResponse;
-import java.io.Serializable;
-import java.util.Random;
 
+import org.primefaces.extensions.component.speedtest.Speedtest;
+import org.primefaces.extensions.event.SpeedTestEvent;
+
+/**
+ * {@link Speedtest} Controller.
+ *
+ * @author Stefan Sibitz ssibitz@me.com
+ */
 @ManagedBean
 @ViewScoped
 public class SpeedTestController implements Serializable {
+
     private static final long serialVersionUID = -5297177555931103300L;
-    private static final String alphabet  = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     private static final Integer OneMB = 1024 * 1024;
     // Size of Download-File (Default=5)
     private Integer sizeInMB = 5;
 
-    public void SaveResults(SpeedTestEvent speedTestEvent) {
-        String msg =    "PingTimeMS       ='"+String.valueOf(speedTestEvent.getPingTimeMS()) + "', \n"+
-                        "Jitter           ='"+String.valueOf(speedTestEvent.getJitterTimeMS()) + "', \n" +
-                        "SpeedMbpsDownload='"+String.valueOf(speedTestEvent.getSpeedMbpsDownload()) + "', \n" +
-                        "SpeedMbpsUpload='"+String.valueOf(speedTestEvent.getSpeedMbpsUpload()) +"'";
-        FacesMessage message =  new FacesMessage(FacesMessage.SEVERITY_INFO, "Saved your speed results:", msg);
+    public void saveResults(SpeedTestEvent speedTestEvent) {
+        String msg = "PingTimeMS       ='" + String.valueOf(speedTestEvent.getPingTimeMS()) + "', \n" +
+                    "Jitter           ='" + String.valueOf(speedTestEvent.getJitterTimeMS()) + "', \n" +
+                    "SpeedMbpsDownload='" + String.valueOf(speedTestEvent.getSpeedMbpsDownload()) + "', \n" +
+                    "SpeedMbpsUpload='" + String.valueOf(speedTestEvent.getSpeedMbpsUpload()) + "'";
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Saved your speed results:", msg);
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
 
-    private String GeneratePayloadFile() {
+    private String generatePayloadFile() {
         // Generate random test string
         StringBuffer randomTestString = new StringBuffer();
-        Integer size2Generate = OneMB*sizeInMB;
-        Integer N = alphabet .length();
+        Integer size2Generate = OneMB * sizeInMB;
+        Integer N = alphabet.length();
         Random r = new Random();
         for (int i = 0; i < size2Generate; i++) {
             randomTestString.append(alphabet.charAt(r.nextInt(N)));
@@ -46,8 +48,8 @@ public class SpeedTestController implements Serializable {
         return randomTestString.toString();
     }
 
-    public void DownloadPayloadFile() {
-        String payloadString = GeneratePayloadFile();
+    public void downloadPayloadFile() {
+        String payloadString = generatePayloadFile();
         try {
             FacesContext facesContext = FacesContext.getCurrentInstance();
             HttpServletResponse response = (HttpServletResponse) facesContext.getExternalContext().getResponse();
@@ -63,7 +65,7 @@ public class SpeedTestController implements Serializable {
         }
     }
 
-    public void UpdateSizeInMB() {
+    public void updateSizeInMB() {
         // Dummy for updating the size on the server...
     }
 
