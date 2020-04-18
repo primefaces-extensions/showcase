79b10620e80cbf1086ff71baec66bdbd50656720
melloware <mellowaredev@gmail.com>
2020-04-18 10:32:04 -0400

    FuzzySearch added Timezone example.

--- src/main/java/org/primefaces/extensions/showcase/controller/fuzzysearch/FuzzySearchController.java
+++ src/main/java/org/primefaces/extensions/showcase/controller/fuzzysearch/FuzzySearchController.java
@@ -1,7 +1,10 @@
 package org.primefaces.extensions.showcase.controller.fuzzysearch;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.List;
+import java.util.TimeZone;
+
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIOutput;
@@ -10,6 +13,7 @@ import javax.faces.event.ActionEvent;
 import javax.faces.event.AjaxBehaviorEvent;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+
 import org.primefaces.extensions.showcase.model.system.AvailableThemes;
 import org.primefaces.extensions.showcase.model.system.Theme;
 
@@ -17,41 +21,74 @@ import org.primefaces.extensions.showcase.model.system.Theme;
 @ViewScoped
 public class FuzzySearchController implements Serializable {
 
-    private static final long serialVersionUID = 20120224L;
+	private static final long serialVersionUID = 20120224L;
 
-    private List<Theme> themes;
-    private Theme selectedTheme;
+	private List<Theme> themes;
+	private Theme selectedTheme;
 
-    @PostConstruct
-    public void init() {
-        themes = AvailableThemes.getInstance().getThemes();
-    }
+	private List<String> timezones;
+	private String selectedTimezone;
 
-    public void onChange(AjaxBehaviorEvent event) {
-        Theme theme = (Theme) ((UIOutput) event.getSource()).getValue();
-        final FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Select fired: Theme is " + theme.getName(), null);
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
+	@PostConstruct
+	public void init() {
+		themes = AvailableThemes.getInstance().getThemes();
+		timezones = Arrays.asList(TimeZone.getAvailableIDs());
+	}
 
-    public void onSubmit(ActionEvent actionEvent) {
-        final FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Select fired: Theme is " + selectedTheme.getName(), null);
-        FacesContext.getCurrentInstance().addMessage(null, msg);
-    }
+	public void onChange(final AjaxBehaviorEvent event) {
+		final Theme theme = (Theme) ((UIOutput) event.getSource()).getValue();
+		final FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,
+				"Select fired: Theme is " + theme.getName(), null);
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
 
-    public List<Theme> getThemes() {
-        return themes;
-    }
+	public void onSubmit(final ActionEvent actionEvent) {
+		FacesMessage msg;
+		if (selectedTheme != null) {
+			msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Theme is: " + selectedTheme.getName(), null);
+		} else {
+			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "No Theme Selected", null);
+		}
+		FacesContext.getCurrentInstance().addMessage(null, msg);
 
-    public void setThemes(List<Theme> themes) {
-        this.themes = themes;
-    }
+		if (selectedTimezone != null) {
+			msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Timezone is: " + selectedTimezone, null);
+		} else {
+			msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "No Timezone Selected", null);
+		}
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
 
-    public Theme getSelectedTheme() {
-        return selectedTheme;
-    }
+	public List<Theme> getThemes() {
+		return themes;
+	}
 
-    public void setSelectedTheme(Theme selectedTheme) {
-        this.selectedTheme = selectedTheme;
-    }
+	public void setThemes(final List<Theme> themes) {
+		this.themes = themes;
+	}
+
+	public Theme getSelectedTheme() {
+		return selectedTheme;
+	}
+
+	public void setSelectedTheme(final Theme selectedTheme) {
+		this.selectedTheme = selectedTheme;
+	}
+
+	public List<String> getTimezones() {
+		return timezones;
+	}
+
+	public void setTimezones(final List<String> timezones) {
+		this.timezones = timezones;
+	}
+
+	public String getSelectedTimezone() {
+		return selectedTimezone;
+	}
+
+	public void setSelectedTimezone(final String selectedTimezone) {
+		this.selectedTimezone = selectedTimezone;
+	}
 
 }
